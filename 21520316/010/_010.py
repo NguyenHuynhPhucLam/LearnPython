41d3483bea83f266e37a4a11451bf32e0f36fcce
kiethongngu <21522263@gm.uit.edu.vn>
2023-09-14 10:07:51 +0700

    Update tới bài 30

---
+++ 21520316/010/_010.py
@@ -0,0 +1,13 @@
+﻿import math
+
+x1, y1 = map(float, input("Nhap (x1 y1): ").split())
+x2, y2 = map(float, input("Nhap (x2 y2): ").split())
+x3, y3 = map(float, input("Nhap (x3 y3): ").split())
+
+ab_distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+bc_distance = math.sqrt((x3 - x2)**2 + (y3 - y2)**2)
+ca_distance = math.sqrt((x1 - x3)**2 + (y1 - y3)**2)
+
+perimeter = ab_distance + bc_distance + ca_distance
+
+print(f"Chu vi cua tam giac ABC la: {perimeter}")
---
+++ 21520316/025/_025.py
@@ -0,0 +1,9 @@
+a = int(input("a: "))
+b = int(input("b: "))
+
+temp = a
+a = b
+b = temp
+
+print(f"a sau khi hoan doi co gia tri: {a}")
+print(f"b sau khi hoan doi co gia tri: {b}")
